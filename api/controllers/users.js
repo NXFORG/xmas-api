9d51259948cb73bae03399ba66c10e3b005167dc
“nxforg” <christopher.anstead@yahoo.co.uk>
2021-11-25 12:18:19 +0000

    add database config to init file

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -39,7 +39,7 @@ async function login (req, res) {
                     token: 'Bearer ' + token
                 })
             } 
-            const secret = 'verysecure';
+            const secret = process.env.SECRET;
 
             const payload = { username: user.username }
 
